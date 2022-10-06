e0e349e07284df3c7c2b23607be81a12ffddc990
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-06 15:01:47 +0200

    adding validation (data insert must be valid)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
 const ticketPrice = document.getElementById("ticket_price");
-
-const km = Number(prompt("Inserisca i chilometri che vuole percorrere"));
-const userAge = Number(prompt("Inserisca la sua età"));
+let  km = Number(prompt("Inserisca i chilometri che vuole percorrere"));
+while(true) {
+    if(km){
+        console.log(km);
+        break;
+    } 
+    km = Number(prompt("Inserisca i chilometri che vuole percorrere"));
+    
+}
+let userAge = Number(prompt("Inserisca la sua età"));
+while(true) {
+    if(userAge){
+        console.log(userAge);
+        break;
+    } 
+    userAge = Number(prompt("Inserisca la sua età"));
+    
+}
 let price = km * 0.21;
 const underAge = 18;
 const overAge = 65;
@@ -9,15 +24,15 @@ const underAgeDiscount = 20;
 const overAgeDiscount = 40;
 
 if (userAge <  underAge) {
-    let discountedPrice;
-    discountedPrice = (price/100) * underAgeDiscount;
-    price = discountedPrice;
+    let discount;
+    discount = (price/100) * underAgeDiscount;
+    price = price - discount;
 } 
 
 if(userAge > overAge) {
-    let discountedPrice;
-    discountedPrice = (price/100) * overAgeDiscount;
-    price = discountedPrice;
+    let discount;
+    discount = (price/100) * overAgeDiscount;
+    price = price - discount;
 }
 price.toString();
 ticketPrice.innerHTML = `Il suo biglietto costa: ${price}€`;
