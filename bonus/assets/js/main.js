6863afff15adf26b8b2c580c594a3214c2b5a602
Mariosam2 <marco.mariosaa@gmail.com>
2022-10-06 16:22:38 +0200

    adding validation

---
+++ bonus/assets/js/main.js
@@ -0,0 +1,30 @@
+const ticketPrice = document.getElementById("ticket_price");
+let  km = Number(prompt("Inserisca i chilometri che vuole percorrere"));
+while (!km){
+    km = Number(prompt("La distanza deve essere un numero"));
+}
+let userAge = Number(prompt("Inserisca la sua età"));
+while (!userAge){
+    userAge = Number(prompt("L'età deve essere un numero"));
+}
+let price = km * 0.21;
+const underAge = 18;
+const overAge = 65;
+const underAgeDiscount = 20;
+const overAgeDiscount = 40;
+
+if (userAge <  underAge) {
+    let discount;
+    discount = (price/100) * underAgeDiscount;
+    price = price - discount;
+} 
+
+if(userAge > overAge) {
+    let discount;
+    discount = (price/100) * overAgeDiscount;
+    price = price - discount;
+}
+
+price = price.toFixed(2);
+price.toString();
+ticketPrice.innerHTML = `Il suo biglietto costa: ${price}€`;
